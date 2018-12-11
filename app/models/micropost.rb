af7c2ca16b2ef9db1cfb0b829efa9ebb25012934
maiko.kirino <sknmik3413@yahoo.co.jp>
2018-12-11 22:27:28 +0000

    update

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,3 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  belongs_to :likes
-  
-  has_many :like_microposts, through: :likes, source: :user
-  has_many :likes
-  validates :content, presence: true, length: { maximum: 255 }
-
 end
